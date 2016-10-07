ffcc9937c21b57bdf5f0e9ebfa7836bd92ada159
madhudskumar <madhudskumar@live.in>
2016-10-08 01:34:52 +0530

    added bot

--- app.js
+++ app.js
@@ -1,35 +1,65 @@
-
 var cfenv = require('cfenv');
 
 var express = require('express'),
-    app = express(),
-    env = process.env.NODE_ENV =  process.env.NODE_ENV || 'development',
-    config = require('./server/config/config.js')[env];
+  app = express(),
+  env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
+  config = require('./server/config/config.js')[env];
 
 require('./server/config/express.js')(app, config);
 app.use('/static', express.static(__dirname + '/public'))
 app.use('/vendor', express.static(__dirname + '/public/vendor'))
 
+var watson = require('watson-developer-cloud');
 
+var conversation = watson.conversation({
+  username: 'c2145a0c-3893-4c1f-87f4-ab4476c6dd92',
+  password: 'IlpOh5RU3nMp',
+  version: 'v1',
+  version_date: '2016-09-20'
+});
+
+
+app.post('/chatResponse', function (req, res) {
+  var text = req.body.text;
+  
+  conversation.message({
+    workspace_id: '86007c58-0d08-4b52-b51a-aca6637e2814',
+    input: {
+      'text': text
+    },
+    context: {}
+  }, function (err, response) {
+    if (err)
+      console.log('error:', err);
+    else{
+      var responseData = {
+        text:response.output.text,
+        confidence:response.intents[0].confidence,
+        intent:response.intents[0].intent
+      };
+      res.json(responseData);
+    }
+  });
+})
 
 app.get('/', function (req, res) {
-    res.render('./../public/clientApp/index');
+  res.render('./../public/clientApp/index');
 })
 
 app.get('/test', function (req, res) {
-    res.json({
-        "test":"success"
-    })
+  res.json({
+    "test": "success"
+  })
 });
 
 app.get("*", function (req, res) {
-    res.redirect(400, '/');
+  res.redirect(400, '/');
 })
 
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+app.listen(appEnv.port, '0.0.0.0', function () {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -6,22 +6,37 @@ var app = angular.module('app', ['ngMaterial'])
     });
 
 app.controller('mainCtrl'
-    , function ($scope) {
+    , function ($scope, $http) {
         $scope.chatInput = {};
         var chatElement = angular.element( document.querySelector( '#chatElement' ) );
         var chatWindow = angular.element( document.querySelector( '#chatWindow' ) )
         $scope.sendText = function (text) {
             if (text) {
+                var data = {
+                    text : text
+                }
+                $http.post('/chatResponse', data)
+                    .then(function(response){
+                        var text = response.data.text[0] || "pardon me please";
+                        chatElement.append("<div class='watson' layout='row' layout-align='start center'><span flex='60'><h1 flex class='fill-container'>" + text + "</h1></div>");
+                        scrollDown();
+                    }, function(err){
+                        console.log(err)
+                    });
+
                 $scope.chatInput = {
                     text: text,
                     id: "u"
                 }
                 console.log($scope.chatInput);
                 chatElement.append("<div class='user' layout='row' layout-align='end center'><span flex='60'><h1 flex class='fill-container'>" + $scope.chatInput.text + "</h1></div>");
-                chatElement.append("<div class='watson' layout='row' layout-align='start center'><span flex='60'><h1 flex class='fill-container'>" + "i know " + "</h1></div>");
             }
             $scope.chatInput = {};
-            document.getElementById("chatWindow").scrollTop = document.getElementById("chatWindow").scrollHeight;
+            scrollDown();
         };
+
+        function scrollDown(){
+            document.getElementById("chatWindow").scrollTop = document.getElementById("chatWindow").scrollHeight;
+        }
     }
 )
